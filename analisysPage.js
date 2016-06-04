51394e6e032c2526691a4468ae6bd72de8f023aa
selear <selear@gmail.com>
2016-06-05 01:28:24 +0800

    完善model
    cheerio的.eq(i)方法是zero based

--- analisysPage.js
+++ analisysPage.js
@@ -19,6 +19,9 @@ fs.readFile(TARGET_FILE, { encoding: 'utf8'}, function(err, utf8html) {
         equipNames = [],
         fenceLength = 0;
 
+    //  TODO countMap目前用于统计每个tr的td, 可以在后续更新中用于新改修数据的校验
+    //  如果数据不对就抛出异常
+    var countMap = { };
 
     $tbody.find('tr').each(function() {
 
@@ -49,35 +52,50 @@ fs.readFile(TARGET_FILE, { encoding: 'utf8'}, function(err, utf8html) {
       //    + $tds.length = 8   - [sun, mon, tue, wed, thu, fri, sat, assist]
       //
       //    + $tds.length = 12  - [develop, improve, cost]
-      //                          [sun, mon, tue, wed, thu, fri, sat]
-      //                          [assist]
+      //                          [sun, mon, tue, wed, thu, fri, sat, assist]
       //
       //    + $tds.length = 17  - [fuel, ammo, steel, bauxite]
       //                          [develop, improve, cost]
-      //                          [sun, mon, tue, wed, thu, fri, sat]
-      //                          [assist, remarks]
+      //                          [sun, mon, tue, wed, thu, fri, sat, assist]
+      //                          [remarks]
       //
       //    + $tds.length = 18  - [fuel, ammo, steel, bauxite]
       //                          [develop, improve, cost]
-      //                          [sun, mon, tue, wed, thu, fri, sat]
-      //                          [assist, remarks]
+      //                          [sun, mon, tue, wed, thu, fri, sat, assist]
+      //                          [remarks]
       // TODO 编写一个数组, 如果出现不同于上述数量的tr, 抛出错误, 并给予提示
       var $tds = $curr.find('td');
       if($tds.first().find('a').length > 0) {
         var eName = $tds.first().text();
         equipNames.push(eName);
 
-        fuel = $tds.eq(3).text();
-        ammo = $tds.eq(4).text();
-        steel = $tds.eq(5).text();
-        bauxite = $tds.eq(6).text();
+        currEquip = new Equip(eName);
 
-        develop = $tds.eq(7).text();
-        improve = $tds.eq(8).text();
-        cost = $tds.eq(9).text();
+        // 油弹钢铝
+        fuel = $tds.eq(2).text();
+        ammo = $tds.eq(3).text();
+        steel = $tds.eq(4).text();
+        bauxite = $tds.eq(5).text();
 
-        // cheerio在读取本地文件后, 忽略了原有结构中的第10个td(该td内没有文字);
-        // 由此, 计数按顺序增加即可
+        var rCost = new ResourceCost(fuel, ammo, steel, bauxite),
+            tar = new ImproveTarget();
+        tar.setResourceCost(rCost);
+
+        currEquip.setImproveTarget(tar);
+
+        // 改修消耗
+        var phaseStr = $tds.eq(1).text();
+        develop = $tds.eq(6).text();
+        improve = $tds.eq(7).text();
+        cost = $tds.eq(8).text();
+        
+        var phase = ImproveCost.whichPhase(phaseStr);
+        var iCost = new ImproveCost();
+        iCost.merge(new ImproveDetail(phase, develop, improve, cost));
+
+        tar.setImproveCost(iCost);
+
+        // cheerio在读取本地文件时, index是zero based, 忽略.eq(9), 此td为空
         sun = ($tds.eq(10).text() === IMPROVABLE) ? true : false;
         mon = ($tds.eq(11).text() === IMPROVABLE) ? true : false;
         tue = ($tds.eq(12).text() === IMPROVABLE) ? true : false;
@@ -89,8 +107,8 @@ fs.readFile(TARGET_FILE, { encoding: 'utf8'}, function(err, utf8html) {
         assist = $tds.eq(17).text();
         remarks = $tds.eq(18).text();
 
-        currCategory.addEquip(new Equip(eName));
-      }
+        currCategory.addEquip(currEquip);
+      } 
 
       if(countMap[$tds.length]) {
         var val = countMap[$tds.length] + 1;
@@ -114,8 +132,8 @@ fs.readFile(TARGET_FILE, { encoding: 'utf8'}, function(err, utf8html) {
 
     });
 
-    //console.log(categories.join());
     console.log(countMap);
+    console.log(categories.join());
     console.log('可改修total : ' + equipNames.length);
     console.log('间隔栏total : ' + fenceLength);
 });
@@ -142,15 +160,111 @@ Category.prototype = {
   }
 }
 
+// Model - Equip
+// 代表某项装备, 其下具有诸多属性:
+// + String name
+// + ImproveTarget
 function Equip(eName) {
   this.name = eName;
+  this.improveTarget = null;
 }
 
 Equip.prototype = {
+  setImproveTarget : function(target) {
+    if(target instanceof ImproveTarget)
+      this.improveTarget = target;
+    else
+      throw new Error();
+  },
   toString : function() {
-    return this.name;
+    return this.name + ' - ' + this.improveTarget.toString();
   }
 }
+
+// Model - ImproveTarget
+// 代表改修装备的方向
+// + ImproveCost improveCost
+// + ResourceCost resourceCost
+// + AssistShip assistShip
+function ImproveTarget() {
+  this.improveCost = null;
+  this.resourceCost = null;
+  this.remark = null;
+}
+
+ImproveTarget.prototype = {
+  setImproveCost : function(iCost) {
+    if(iCost instanceof ImproveCost)
+      this.improveCost = iCost;
+    else
+      throw new Error();
+  },
+  setResourceCost : function(rCost) {
+    if(rCost instanceof ResourceCost)
+      this.resourceCost = rCost;
+    else
+      throw new Error();
+  },
+  toString : function() {
+    return this.improveCost.toString() + ', ' + this.resourceCost.toString();
+  }
+};
+
+// Model - ImproveCost
+function ImproveCost() {
+  this.cost = [];
+}
+
+ImproveCost.whichPhase = function(phaseStr) {
+  switch(phaseStr) {
+    case '初期':
+      return 0;
+      break;
+    case '★6':
+      return 1;
+      break;
+    case '★max':
+      return 2;
+      break;
+    default:
+      throw new Error(phaseStr);
+      break;
+  }
+}
+
+ImproveCost.prototype = {
+  merge : function(detail) {
+    if(detail instanceof ImproveDetail)
+      this.cost[detail.phase] = detail;
+    else
+      throw new Error();
+  },
+  toString : function() {
+    return this.cost.join();
+  }
+};
+
+function ImproveDetail(phase, improve, develop, cost) {
+  this.phase = phase
+  this.cost = [improve, develop, cost]
+}
+
+ImproveDetail.prototype = {
+  toString : function() {
+    return '[' + this.cost.join(', ') + ']';
+  }
+};
+
+// Model - ResourceCost
+function ResourceCost(fuel, ammo, steel, bauxite) {
+  this.cost = [fuel, ammo, steel, bauxite];
+}
+
+ResourceCost.prototype = {
+  toString : function() {
+    return '[' + this.cost.join('\/') + ']';
+  }
+};
 
 function banner(bannerText) {
   var DELIMITER = '-',
@@ -165,3 +279,4 @@ function banner(bannerText) {
 
   console.log([line, fixed, line].join('\n'));
 }
+
