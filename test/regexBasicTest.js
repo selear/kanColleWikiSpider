24ea842c1fdf5c2582bbdd328d442f87b17c9575
selear <selear@gmail.com>
2017-03-01 00:49:38 +0800

    添加并完善最后的测试条件 -/-

--- test/regexBasicTest.js
+++ test/regexBasicTest.js
@@ -6,7 +6,6 @@ const assert = require('assert');
 //   两组数字测试中, 正则表达式为
 //      /\b([1-9]\d|\d)\/([1-9]\d|[1-9])\b|^-\/-$/gm
 //   在本测试环境中, 每个测试条件都是单行测试, 由此不需要多行匹配模式m, ^, $
-// 变为 /\b([1-9]\d|\d)\/([123456789]\d|[123456789])\b|-\/-/g
 /*
      0/0
      0/1    -- T
@@ -81,7 +80,9 @@ const assert = require('assert');
      -/--
      --/--
  */
-const TWO_NUMS_REG = /\b([1-9]\d|\d)\/([1-9]\d|[1-9])\b|-\/-/;
+// 最初regexp - /\b([1-9]\d|\d)\/([1-9]\d|[1-9])\b|-\/-/
+// 优化regexp - /\b([1-9]?\d)\/([1-9]\d?)\b|^-\/-$/m
+const TWO_NUMS_REGEXP = /\b([1-9]?\d)\/([1-9]\d?)\b|^-\/-$/m;
 
 const FOR_TEST = {
   'head_0'           : ['0/0','0/1','0/9','0/10','0/99','0/100','0/999'],
@@ -91,8 +92,9 @@ const FOR_TEST = {
   'head_99'          : ['99/0','99/1','99/9','99/10','99/99','99/100','99/999'],
   'head_99_Alphabet' : ['99/1a','99/9b','99/10c','99/99d','99/100e','99/999f'],
   'head_000'         : ['000/0','000/1','000/10','000/99','000/100','000/999'],
-  'head_010'         : [],
-  'head_999'         : []
+  'head_010'         : ['010/0','010/1','010/10','010/99','010/100','010/999'],
+  'head_999'         : ['100/0','100/1','100/10','100/99','100/100','100/999'],
+  'special'          : ['-/-','-/ -','- /-','--/-','-/--','--/--']
 };
 
 const EXPECTED = {
@@ -104,14 +106,15 @@ const EXPECTED = {
   'head_99_Alphabet' : [],
   'head_000'         : [],
   'head_010'         : [],
-  'head_999'         : []
+  'head_999'         : [],
+  'special'          : ['-/-']
 };
 
 
 var validateTwoNums = function(target) {
 
   var validElemArr = FOR_TEST[target].filter(function(elem) {
-    return TWO_NUMS_REG.test(elem);
+    return TWO_NUMS_REGEXP.test(elem);
   });
   assert.deepEqual(validElemArr, EXPECTED[target], 'error in [' + target + ']');
 };
@@ -125,3 +128,4 @@ validateTwoNums('head_99_Alphabet');
 validateTwoNums('head_000');
 validateTwoNums('head_010');
 validateTwoNums('head_999');
+validateTwoNums('special');
