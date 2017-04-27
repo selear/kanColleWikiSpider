8904c62260e95fd2811c388e2b825a19c0fbf4b1
selear <selear@gmail.com>
2017-04-27 11:59:05 +0800

    对原始数据进行分类组合，减少对外显示的类别总数

--- formatter.js
+++ formatter.js
@@ -4,7 +4,7 @@ const async = require('async');
 async.waterfall([
     openFile,
     parseToJson,
-    doExport
+    classifyEquip
   ],
   function(err, result) {
     if(err)
@@ -39,13 +39,61 @@ function parseToJson(fileContent, callback) {
   }
 }
 
-function doExport(json, callback) {
+function classifyEquip(json, callback) {
 
-  if(json.categories) {
-    
-    
-    callback(null, json.categories[0]);
+  if(!json.categories) {
+
+    callback(new Error('fileContent has NO "categories", or WRONG data.'));
+  } else {
+
+    let categoryMeta = objectify(json.categories);
+    let classfiedMap = classifyCategory(categoryMeta);
+
+    callback(null, classfiedMap);
   }
-  else
-    callback(new Error('fileContent has NO "categories"'));
+
+  // 将数组变为对象
+  function objectify(categoryArr) {
+
+    let categoryMeta = {};
+    categoryArr.forEach(function(category) {
+      categoryMeta[category.cName] = category.equipArr;
+    });
+
+    return categoryMeta;
+  }
+
+  // 组合不同的分类装备到"组合"中
+  function classifyCategory(categoryMeta) {
+
+    let classifyMap = new Map([
+      ['小口径主炮/鱼雷', ['小口径主砲', '魚雷']],
+      ['中口径主炮/副炮', ['中口径主砲', '副砲']],
+      ['大口径主炮/强化弹', ['大口径主砲', '対艦強化弾']],
+      ['舰战', ['艦上戦闘機']],
+      ['舰爆/舰侦', ['艦上爆撃機', '艦上偵察機']],
+      ['水侦/水战', ['水上偵察機', '水上戦闘機']],
+      ['电探', ['電探']],
+      ['机枪/高射装置', ['対空機銃', '高射装置']],
+      ['声纳/爆雷/潜水艇装备', ['ソナ｜', '爆雷', '潜水艦装備']],
+      ['登陆艇/探照灯/装甲带/动力', ['上陸用舟艇', '探照灯', 'バルジ', '機関部強化']]
+    ]);
+
+    let classfied = new Map();
+    classifyMap.forEach((value, key) => {
+      let arr = [];
+      value.forEach((name) => {
+        categoryMeta[name].forEach((equip) => {
+          arr.push(equip);
+        });
+      });
+      classfied.set(key, arr);
+    });
+
+    return classfied;
+  }
+}
+
+function doExport(json, callback) {
+  //输出数据
 }
